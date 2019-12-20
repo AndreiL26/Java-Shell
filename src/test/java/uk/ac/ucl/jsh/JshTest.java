8a13d7a12a7e1f7921d0e7cba5ebc93518454dab
Alexandru Niculae <niculae.alexandru1@gmail.com>
2019-12-20 09:56:12 +0000

    JstTest fixed

--- src/test/java/uk/ac/ucl/jsh/JshTest.java
+++ src/test/java/uk/ac/ucl/jsh/JshTest.java
@@ -9,14 +9,8 @@ public class JshTest {
 
     @Test
     public void testJsh() throws Exception {
-<<<<<<< HEAD
-        Jsh.eval("echo mama a\"m\"a", System.out);
-        //Jsh.eval("echo jas", System.out);
-        assert(true);
-=======
        
 
->>>>>>> fc6ca1f7aa6eb537f0a0d8562cc87edb0ee59d53
     }
 
 }
