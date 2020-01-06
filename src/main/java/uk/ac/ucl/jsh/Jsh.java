f08323b055b0cff1242609e497e5462dd25f6fd7
Andrei Lazar <lazarandrei260@gmail.com>
2020-01-06 11:11:16 +0000

    Final commit

--- src/main/java/uk/ac/ucl/jsh/Jsh.java
+++ src/main/java/uk/ac/ucl/jsh/Jsh.java
@@ -8,20 +8,45 @@ import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+/**
+ *  Class that encapsulates the Jsh Terminal
+ */
 public class Jsh {
+    /**
+     * Utility string that ensures that the paths created by the find function are not system-dependent
+     */
     public final static String fileSeparator = System.getProperty("file.separator");
+    /**
+     * Utility string that ensures that the output of applications is not system-dependent
+     */
     public final static String lineSeparator = System.getProperty("line.separator");
-
+    /**
+     * ArrayList holding the history of commands, used by the History Application
+     */
     private static ArrayList<String> history = new ArrayList<>();
 
+    /**
+     * Getter function for the history ArrayList
+     * 
+     * @return  The Arraylist representing the history of commands
+     */
     public static ArrayList<String> getHistory() {
         return history;
     }
 
+    /**
+     * Utility function used to clear the commands history
+     */
     public static void clearHistory() {
         history.clear();
     }
 
+    /**
+     * Function that evaluates the command line given as argument and writes it to the output stream
+     * 
+     * @param cmdline       String representing the command line
+     * @param outputStream  Output stream used to write to
+     */
     public static void eval(String cmdline, OutputStream outputStream) {
         Node cmdTree = Parser.parserCmdLine(cmdline);
         try {
@@ -31,6 +56,12 @@ public class Jsh {
         }
     }
 
+    /**
+     *The main function which makes the appropriate calls for Jsh to operate
+     * and that loops continually until exited.
+     * 
+     * @param args       The arguments that are passed in after running the Jsh based on which the Jsh runs in interactive mode or not
+     */
     public static void main(String[] args) throws Exception {
         if (args.length > 0) {
             if (args.length != 2) {
